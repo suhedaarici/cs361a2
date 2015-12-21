952fd7c718d0f7456b24fdf5cb9eff036718f951
suhedaarici <suhedaarici@std.sehir.edu.tr>
2015-12-21 14:15:19 +0200

    Update models.py

--- assignment2/models.py
+++ assignment2/models.py
@@ -9,7 +9,7 @@ class Course(models.Model):
     course_name = models.CharField(max_length=30)
     course_code = models.CharField(max_length=50)
     course_classroom = models.CharField(max_length=60)
-    course_times = models.DateField(max_length=30)
+    course_times = models.DateField()
     course_student = models.ManyToManyField(Student)
     course_teacher = models.OneToOneField(Teacher)
 
